26a45f9cd1a1832bb33e9d88c25dd4a8b1b82500
eliiinzooo <elise.collignon@edu.ece.fr>
2025-02-10 10:14:28 +0100

    feat: ajout de la configuration initiale React avec pages Home et About

--- stella/src/js/App.js
+++ stella/src/js/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
+import Home from '../pages/Home';
+import About from '../pages/About';
 
-// Importation des pages
-import Home from './pages/Home';
-import About from './pages/About';
+
 
 // Composant de navigation (facultatif, mais utile pour naviguer)
 import { Link } from 'react-router-dom';
@@ -14,16 +13,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-
-          {/* Navigation */}
-          <nav>
-            <Link to="/" className="App-link">Home</Link>
-            <Link to="/about" className="App-link">About</Link>
-          </nav>
-        </header>
 
         {/* Configuration des Routes */}
         <Routes>
