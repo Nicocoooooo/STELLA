e1766e5f84276503972d1816d9c74f388263e4a0
alexandre.vachez <alexandre.vachez@edu.ece.fr>
2025-03-31 19:58:29 +0200

    léger fix 2.0

--- src/js/App.js
+++ src/js/App.js
@@ -1,34 +1,33 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { QuizProvider } from '../context/QuizContext';
-import '../styles/App.css';
-import SupabaseTest from '../components/SupabaseTest';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QuizProvider } from "../context/QuizContext";
+import "../styles/App.css";
+import SupabaseTest from "../components/SupabaseTest";
 
 // Import des pages
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Quiz from '../pages/Quiz';
-import Questions from '../pages/Questions';
-import BudgetQuestions from '../pages/BudgetQuestions';
-import PreferencesQuestions from '../pages/PreferencesQuestions';
-import QuizConfirmation from '../pages/QuizConfirmation';
-import SignUp from '../pages/SignUp';
-import SignUp2 from '../pages/SignUp2';
-import SignUp3 from '../pages/SignUp3';
-import Login from '../pages/Login';
-import PastTrips from '../pages/PastTrips'; // Import de la page Mes Anciens Voyages
-import Dashboard from '../pages/Dashboard';
-import PastTripDetail from '../pages/PastTripDetail'; // Import de la page Détail de l'ancien Voyage
-import ProtectedRoute from '../components/ProtectedRoute'; // Import du composant ProtectedRoute
-import DestinationDetail from '../pages/DestinationDetail';
-import UserProfil from '../pages/UserProfil';
-import Help from '../pages/Help';
-import Contact from '../pages/Contact';
-import LegalMentions from '../pages/LegalMentions';
-import FAQ from '../pages/Faq';
-import Ask from '../pages/Ask';
-
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Quiz from "../pages/Quiz";
+import Questions from "../pages/Questions";
+import BudgetQuestions from "../pages/BudgetQuestions";
+import PreferencesQuestions from "../pages/PreferencesQuestions";
+import QuizConfirmation from "../pages/QuizConfirmation";
+import SignUp from "../pages/SignUp";
+import SignUp2 from "../pages/SignUp2";
+import SignUp3 from "../pages/SignUp3";
+import Login from "../pages/Login";
+import PastTrips from "../pages/PastTrips"; // Import de la page Mes Anciens Voyages
+import Dashboard from "../pages/Dashboard";
+import PastTripDetail from "../pages/PastTripDetail"; // Import de la page Détail de l'ancien Voyage
+import ProtectedRoute from "../components/ProtectedRoute"; // Import du composant ProtectedRoute
+import DestinationDetail from "../pages/DestinationDetail";
+import UserProfil from "../pages/UserProfil";
+import Help from "../pages/Help";
+import Contact from "../pages/Contact";
+import LegalMentions from "../pages/LegalMentions";
+import FAQ from "../pages/Faq";
+import Ask from "../pages/Ask";
 
 import ComposeVoyage from "../pages/ComposeVoyage";
 import HotelsPage from "../pages/HotelsPage";
@@ -59,17 +58,18 @@ function App() {
               <Route path="/quiz" element={<Quiz />} />
               <Route path="/quiz/questions" element={<Questions />} />
               <Route path="/quiz/budget" element={<BudgetQuestions />} />
-              <Route path="/quiz/preferences" element={<PreferencesQuestions />} />
+              <Route
+                path="/quiz/preferences"
+                element={<PreferencesQuestions />}
+              />
               <Route path="/test" element={<SupabaseTest />} />
               <Route path="/quiz/confirmation" element={<QuizConfirmation />} />
               <Route path="/destination/:id" element={<DestinationDetail />} />
 
-
-               {/* Routes Composez Voyage*/}
-              <Route path="/ComposeVoyage" element={<ComposeVoyage />} /> 
-              <Route path="/HotelsPage" element={<HotelsPage />} /> 
-              <Route path="/PlanningPage1" element={<PlanningPage1 />} /> 
-
+              {/* Routes Composez Voyage*/}
+              <Route path="/ComposeVoyage" element={<ComposeVoyage />} />
+              <Route path="/HotelsPage" element={<HotelsPage />} />
+              <Route path="/PlanningPage1" element={<PlanningPage1 />} />
 
               {/* Routes d'inscription et connexion */}
               <Route path="/signup" element={<SignUp />} />
@@ -79,32 +79,32 @@ function App() {
               <Route path="/user-profil" element={<UserProfil />} />
 
               {/* Routes protégées */}
-              <Route 
-                path="/past-trips" 
+              <Route
+                path="/past-trips"
                 element={
                   <ProtectedRoute>
                     <PastTrips />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/dashboard" 
+              <Route
+                path="/dashboard"
                 element={
                   <ProtectedRoute>
-                    <Dashboard/>
+                    <Dashboard />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/past-trips/:id" 
+              <Route
+                path="/past-trips/:id"
                 element={
                   <ProtectedRoute>
                     <PastTripDetail />
                   </ProtectedRoute>
-                } 
+                }
               />
-              
             </Routes>
+            {/* Composant d'analyse de Vercel Analytics */}
             <Analytics />
           </div>
         </Suspense>
